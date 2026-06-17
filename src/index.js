21faec6e4057aceeecaed05bbc64e3a3098b46f2
Adamya Chauhan <adamya1332004@gmail.com>
2026-06-17 22:35:35 +0530

    rabbitmq

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const { PORT } = require('./config/serverConfig');
 const { sendBasicEmail} = require('./services/email-service');
 const job = require('./utils/jobs');
 const TicketController = require('./controller/ticket-controller');
+const { createChannel } = require('./utils/messageQueue');
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
     app.post('/api/v1/tickets', TicketController.create);
+    const channel = await createChannel();
 
     app.listen(PORT, () => {
         console.log(`Server started listening on ${PORT}`);
